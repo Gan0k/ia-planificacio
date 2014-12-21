ab5defce4053c585a6146edd25f537631d1ee2a7
srgrr <srgrr@github.com>
2014-12-22 00:38:11 +0100

    esborrats .out inneccesaris

--- ext3/experimento-incremental/createTests.py
+++ ext3/experimento-incremental/createTests.py
@@ -1,5 +1,5 @@
 import os
 
-for i in range(1,21):
+for i in range(1,6):
     ncs = i*5
     os.system("./generador " + `ncs` + " 1 1 " + `(ncs/2)` + " 100 10000 10 1 test" + `i` + " > tests/test" + `i` + ".pddl");
