9bf45e379c25ede1f344b39ba8b6babc7593fa32
srgrr <srgrr@github.com>
2014-12-22 01:25:39 +0100

    tests definitius fets

--- ext3/experimento-incremental/createTests.py
+++ ext3/experimento-incremental/createTests.py
@@ -1,5 +1,6 @@
 import os
 
-for i in range(1,6):
-    ncs = i*5
-    os.system("./generador " + `ncs` + " 1 1 " + `(ncs/2)` + " 100 10000 10 1 test" + `i` + " > tests/test" + `i` + ".pddl");
+for i in range(1,30):
+    ncs = i
+    os.system("./generador " + `ncs` + " 1 1 " + `ncs` + " 100 10000 20 1 test" + `i` + " > tests/test" + `i` + ".pddl");
+    
--- ext3/experimento-incremental/runTests.py
+++ ext3/experimento-incremental/runTests.py
@@ -1,5 +1,5 @@
 import os 
 
-for i in range(1,6):
-    print("~/Metric-FF/ff -o ../travel.pddl -f tests/test "+`i` + ".pddl -O > traces/trace"+`i`+".out")
+for i in range(1,30):
+    print("~/Metric-FF/ff -o ../travel.pddl -f tests/test"+`i` + ".pddl -O > traces/trace"+`i`+".out")
     os.system("~/Metric-FF/ff -o ../travel.pddl -f tests/test"+`i` + ".pddl -O > traces/trace"+`i`+".out")
